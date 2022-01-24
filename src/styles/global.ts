894600b94d851a670fed8d992c4b4a2e0538ff3d
bepyan <bepyan@naver.com>
2022-01-24 16:17:56 +0900

    chore. font-face css로 적용

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,12 @@
+import './font-face.css';
 import { globalCss } from '@stitches/react';
 
 export const globalStyles = globalCss({
-  '@font-face': {
+  '*': {
+    margin: 0,
+    padding: 0,
     fontFamily: 'Noto Sans KR',
-    src: 'local(../assets/fonts/NotoSansKR-Bold), local(../assets/fonts/NotoSansKR-Regular)',
   },
-  '*': { margin: 0, padding: 0, fontFamily: 'Noto Sans KR' },
   'ol, ul, li, menu': {
     listStyle: 'none',
   },
--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,14 @@ module.exports = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
